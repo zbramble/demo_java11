98ca3b7d7b7997a9f5857ee7859e52e850ad0f97
David Zhang <zb.ramble@gmail.com>
2023-02-10 14:22:09 -0800

    init java 11 spring boot

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.8"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+springBoot {
+	mainClass.set("com.example.demo.DemoJavaApplication")
+}
+
+tasks.withType<Jar>() {
+	duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+tasks.named<Jar>("jar") {
+	enabled = false
+}
---
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String Welcome() {
+        String welcomeMsg = "Welcome to Java 11 Project!";
+        return welcomeMsg;
+    }
+}
