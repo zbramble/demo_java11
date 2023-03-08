9d2ae1e6e13dda41e38c9c8844e8c880a96c38e0
David Zhang <zb.ramble@gmail.com>
2023-03-08 10:13:34 -0800

    update welcome msg

--- src/main/java/com/example/demo/WelcomeController.java
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -8,7 +8,7 @@ public class WelcomeController {
 
     @GetMapping("/welcome")
     public String Welcome() {
-        String welcomeMsg = "Welcome to Java 11 Project!";
+        String welcomeMsg = "Welcome to David Zhang's Java 11 Project!";
 
         //TODO Test Remove Git SSH Key
         return welcomeMsg;
